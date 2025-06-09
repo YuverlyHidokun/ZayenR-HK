4aa746fc249a3889f9d040b95f4633c565e1218c
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2025-06-09 11:33:58 -0500

    agregando la funcionalidad de los correos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
-import { Home } from './pages/home'
-import { Login } from './pages/Login'
-import { Register } from './pages/Registro'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Registro';
+
+import { ForgotPassword } from './pages/ForgotPassword';
+import { NewPassword } from './pages/NewPassword';
+import { NotFound } from './pages/NotFound'; // Para la ruta 404
+import {ResetPassword} from './pages/ResetPassword'
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home/>}/>
-                <Route path='login' element={<Login/>}/>
-                <Route path='registro' element={<Register/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="registro" element={<Register />} />
+
+        <Route path="recuperar" element={<ForgotPassword />} />
+        <Route path="reset/:token" element={<ResetPassword />} />
+        <Route path="nueva-contrasena" element={<NewPassword />} />
+
+
+        {/* Ruta para cualquier otra que no exista */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
