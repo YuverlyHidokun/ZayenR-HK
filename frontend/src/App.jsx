6426041c485c2bbe504862c3f5d14431ce3038c0
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2025-06-09 12:36:03 -0500

    pagina de verificar token inplementada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import { Home } from './pages/home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Registro';
+import { ConfirmAccount } from './pages/ConfirmAccount';
 
 import { ForgotPassword } from './pages/ForgotPassword';
 import { NewPassword } from './pages/NewPassword';
-import { NotFound } from './pages/NotFound'; // Para la ruta 404
-import {ResetPassword} from './pages/ResetPassword'
+import { NotFound } from './pages/NotFound'; 
+import { ResetPassword } from './pages/ResetPassword';
+
+import Dashboard from './layouts/Dashboard';
+/*
+import Perfil from './Perfil';
+import Listar from './Listar';
+import Crear from './Crear';
+import Chat from './Chat';
+*/
 
 function App() {
   return (
@@ -16,13 +26,15 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="registro" element={<Register />} />
-
+        <Route path="confirmar/:token" element={<ConfirmAccount />} />
         <Route path="recuperar" element={<ForgotPassword />} />
         <Route path="reset/:token" element={<ResetPassword />} />
         <Route path="nueva-contrasena" element={<NewPassword />} />
+        <Route path="dashboard" element={<Dashboard />} />
 
+        
 
-        {/* Ruta para cualquier otra que no exista */}
+        {/* Ruta 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
