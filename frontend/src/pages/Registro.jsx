260e91303d14813edc3808287d43a6db2f2832b8
AlissonMunoz78 <alisson.munoz@epn.edu.ec>
2025-06-11 21:09:32 -0500

    Co-authored-by: Yuverly Hidokun <YuverlyHidokun@users.noreply.github.com>

--- frontend/src/pages/Registro.jsx
+++ frontend/src/pages/Registro.jsx
@@ -159,7 +159,7 @@ export const Register = () => {
       {/* Imagen al lado derecho */}
       <div className="w-1/2 hidden md:block">
         <img
-          src="src/assets/dino2.jpg"
+          src="src/assets/dino-repair.png"
           alt="Registro Museo"
           className="w-full h-full object-cover"
         />
